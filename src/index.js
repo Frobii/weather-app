acace2fc0fa02731e7b1dc481bf6edba15ad49d0
frobee <frobi.webdev@outlook.com>
2023-09-05 20:40:10 +0930

    fetch-three-days-of-weather-and-alter-current-day-processing

--- src/index.js
+++ src/index.js
@@ -1,33 +1,43 @@
 import './style.css';
 
-async function fetchLocationData(location) {
-  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=595f8949c2a74450b66224031231408 &q=${location}&aqi=no`);
+async function fetchThreeDayForecast(location) {
+  const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=595f8949c2a74450b66224031231408&q=${location}&days=5`);
   const fetchedData = await response.json();
 
   return fetchedData;
 }
 
 async function processLocationData(fetchedData) {
-  const currentData = fetchedData.current;
-  const locationData = fetchedData.location;
+  const currentWeather = fetchedData.current;
+  const forecastArray = await fetchedData.forecast.forecastday;
+  const todaysForecast = await forecastArray[0].day;
 
-  const description = currentData.condition.text;
-  const locationName = locationData.name;
-  const dateTime = currentData.last_updated.split(' ');
+  console.log('current weather', currentWeather);
+  console.log('forecast data', forecastArray);
+  console.log('todays forecast', todaysForecast);
+
+  const currentCondition = currentWeather.condition.text;
+  const currentConditionIcon = currentWeather.condition.icon;
+  const forecastCondition = todaysForecast.condition.text;
+  const forecastConditionIcon = todaysForecast.condition.icon;
+  const locationName = fetchedData.location.name;
+  const dateTime = currentWeather.last_updated.split(' ');
   const date = dateTime[0];
   const time = dateTime[1];
-  const currentTempC = currentData.temp_c;
-  const currentTempF = currentData.temp_f;
-  const { humidity } = currentData;
-  const feelsLikeC = currentData.feelslike_c;
-  const feelsLikeF = currentData.feelslike_f;
-  // const chanceOfRain =
-  const windSpeed = currentData.wind_kph;
-  // const sunrise = currentData.
-  // const sunset = currentData.
-  const uvIndex = currentData.uv;
+  const currentTempC = currentWeather.temp_c;
+  const currentTempF = currentWeather.temp_f;
+  const { humidity } = currentWeather;
+  const feelsLikeC = currentWeather.feelslike_c;
+  const feelsLikeF = currentWeather.feelslike_f;
+  const precipation = currentWeather.precip_in;
+  const windSpeed = currentWeather.wind_kph;
+  const uvIndex = currentWeather.uv;
+
   return {
-    description,
+    currentCondition,
+    currentConditionIcon,
+    forecastCondition,
+    forecastConditionIcon,
     locationName,
     date,
     time,
@@ -36,13 +46,14 @@ async function processLocationData(fetchedData) {
     humidity,
     feelsLikeC,
     feelsLikeF,
+    precipation,
     windSpeed,
     uvIndex,
   };
 }
 
-const fetchedData = await fetchLocationData('Atlantis');
+const fetchedData = await fetchThreeDayForecast('Adelaide');
 const processedData = await processLocationData(fetchedData);
 
-console.log(fetchedData);
-console.log(processedData);
+// console.log(fetchedData);
+console.log('processed current weather', processedData);
