7a54de140994c613cd57b0029bb5302ca56a84d0
frobee <frobi.webdev@outlook.com>
2023-09-09 23:08:38 +0930

    setup-a-html-template-to-work-with-the-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: './src/scripts/index.js',
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Development',
